9223ff9c373b6eba61c5291ad99ea431d0710be2
LinnikV <linnikvitek@ukr.net>
2022-05-09 21:32:47 +0300

    add navbar and devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
+  root 'static_page#home'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
