e2f7083019da27026267efc85ca6900f7c1aa08d
LinnikV <linnikvitek@ukr.net>
2022-05-10 15:01:52 +0300

    scaffold folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :folders
   root 'static_page#home'
   devise_for :users
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_09_173713) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_10_111327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "folders", force: :cascade do |t|
+    t.string "title", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_folders_on_title", unique: true
+    t.index ["user_id"], name: "index_folders_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -27,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_09_173713) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "folders", "users"
 end
